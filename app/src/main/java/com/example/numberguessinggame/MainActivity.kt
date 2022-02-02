5c940d1c2de4eb670c2eba1dd1c4639d9c51c5e5
Kotchakorn Surin <6210612674@student.tu.ac.th>
2022-02-03 04:31:57 +0700

    First Commit

--- app/src/main/java/com/example/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/example/numberguessinggame/MainActivity.kt
@@ -1,33 +1,95 @@
 package com.example.numberguessinggame
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.*
 import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textView: TextView
-    private lateinit var textView2: TextView
-    private lateinit var editTextNumber: EditText
-    private lateinit var imageButton: ImageButton
-    private lateinit var imageButton2: ImageButton
+    private lateinit var tvHint: TextView
+    private lateinit var tvHead: TextView
+    private lateinit var tvWrong: TextView
+    private lateinit var etInput: EditText
+    private lateinit var ibCheck: ImageButton
+    private lateinit var ibReset: ImageButton
+    private lateinit var ibGiveUp: ImageButton
 
     var random: Int = nextInt(1, 1000)
+    var wrong: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textView = findViewById(R.id.textView)
-        textView2 = findViewById(R.id.textView2)
-        editTextNumber = findViewById(R.id.editTextNumber)
-        imageButton = findViewById(R.id.imageButton)
-        imageButton2 = findViewById(R.id.imageButton2)
+        tvHint = findViewById(R.id.tvHint)
+        tvHead = findViewById(R.id.tvHead)
+        tvWrong= findViewById(R.id.tvWrong)
+        etInput = findViewById(R.id.etInput)
+        ibCheck = findViewById(R.id.ibCheck)
+        ibReset = findViewById(R.id.ibReset)
+        ibGiveUp = findViewById(R.id.ibGiveUp)
 
-        textView2.text = "Enter your guess"
+        tvHead.text = "Enter your guess"
+        tvWrong.text = "wrong answer: $wrong"
 
+        ibCheck.setOnClickListener {
+            val num: Int = etInput.text.toString().toInt()
+            checkAnswer(num)
+        }
 
+        ibReset.setOnClickListener {
+            reset()
+        }
+
+        ibGiveUp.setOnClickListener {
+            tvHead.text = "You lose!\nAnswer is $random"
+            tvHint.text = "Reset to play again"
+            etInput.text.clear()
+        }
+    }
+
+    private fun checkAnswer(num: Int) {
+        when {
+            num < random -> {
+                wrong++
+                tvWrong.text = "wrong answer: $wrong"
+                tvHead.text = "Try again!"
+                tvHint.text = "Hint: It's higher!"
+                Toast.makeText(this,"Wrong!", Toast.LENGTH_SHORT).show();
+                etInput.text.clear()
+            }
+            num > random -> {
+                wrong++
+                tvWrong.text = "wrong answer: $wrong"
+                tvHead.text = "Try again!"
+                tvHint.text = "Hint: It's lower!"
+                Toast.makeText(this,"Wrong!", Toast.LENGTH_SHORT).show();
+                etInput.text.clear()
+            }
+            else -> {
+                tvWrong.text = ""
+                tvHead.text = "Congratulations!"
+                tvHint.text = "wrong answer: $wrong"
+                Toast.makeText(this,"Correct!", Toast.LENGTH_SHORT).show();
+                etInput.text.clear()
+            }
+        }
+    }
+
+    private fun reset() {
+        random = nextInt(1,1000)
+        wrong = 0
+        tvHead.text = "Enter your guess"
+        tvHint.text = ""
+        tvWrong.text = "wrong answer: $wrong"
+        etInput.text.clear()
     }
 }
